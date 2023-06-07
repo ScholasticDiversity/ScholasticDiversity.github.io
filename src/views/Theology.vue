9110e83d75b5d490c5e4f4b5291005496dc297a1
Christian Seibold <christian.seibold@outlook.com>
2023-06-07 04:38:34 -0500

    Work on Theology overview page

--- src/views/Theology.vue
+++ src/views/Theology.vue
@@ -2,12 +2,161 @@
 	<h1 class="text-h4 text-center">Theology Overview</h1>
 	<v-divider class="mb-6"></v-divider>
 	<v-timeline direction="vertical">
-		
+		<v-timeline-item dot-color="black">
+			<div class="text-h6 mt-n1">c. 551 BCE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Confucius</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="black">
+			<div class="text-h6 mt-n1">c. 624-470 BCE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Pre-Socratic</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="black">
+			<div class="text-h6 mt-n1">c. 428/427 or 424/423 BCE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Plato</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="black">
+			<div class="text-h6 mt-n1">c. 384 BCE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Aristotle</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="black">
+			<div class="text-h6 mt-n1">c. 334 BCE-121 CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Stoics</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="black">
+			<div class="text-h6 mt-n1">c. 90 BCE to 3rd Century CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Middle Platonism: Philo of Alexandria, Plutarch of Chaeronea</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="purple">
+			<div class="text-h6 mt-n1">c. 100-451 CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Patristics: Justin Martyr, Athanasius of Alexandria, Basil of Caesarea, Gregory of Nazianzus, John Crysostom, Cyril of Alexandria, Maximus the Confessor, Tertullian, Cyprian, Jerome, Ambrose of Milan, Augustine of Hippo, Gregory the Great</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="blue-darken-1">
+			<div class="text-h6 mt-n1">c. 200 CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Mishnah, compiled by Judah HaNasi</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="blue-darken-1">
+			<div class="text-h6 mt-n1">c. 200-400 CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Jerusalem Talmud (Yerushalmi)</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="blue-darken-1">
+			<div class="text-h6 mt-n1">c. 200-600 CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Babylonian Talmud (Bavli)</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="black">
+			<div class="text-h6 mt-n1">c. 204-1501 CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Neoplatonists: Plotinus, Aedesia, Avempace, Solomon ben Judah (Avicebrol), Anselm, Rabbi Moses ben Jacob ibn Ezra (Ha-Sallaḥ), Abraham Ibn Ezra (Abenezra), St. Bernard of Clairvaux, Hildegard of Bingen</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="blue-darken-1">
+			<div class="text-h6 mt-n1">c. 1075 CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Judah HaLevi</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="green">
+			<div class="text-h6 mt-n1">c. 1075 CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Muslim Aristotelians: Avicenna (Ibn Sina), Averroes (Ibn Rushd)</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="purple">
+			<div class="text-h6 mt-n1">c. 1075 CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Aristotelian Scholastics: Héloïse d'Argenteuil, Peter Abelard, Peter Lombard, Bonaventure, Thomas Aquinas, John Duns Scotus</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="blue-darken-1">
+			<div class="text-h6 mt-n1">c. 1075 CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Jewish Aristotelians: Maimonides, David Kimhi (Radak), Levi ben Gershon (Gersonides or Ralbag)</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="blue-darken-1">
+			<div class="text-h6 mt-n1">c. 1170-1180 CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Mishneh Torah, written by Maimonides.</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="blue-darken-1">
+			<div class="text-h6 mt-n1">c. 1194 CE</div>
+			<v-divider class="mb-4"></v-divider>
+			<p class="text-body">Nachmanides (Anti-Aristotle).</p>
+		</v-timeline-item>
+		<v-timeline-item dot-color="red">
+			<div>
+				<div class="text-h6 mt-n1">1517 CE</div>
+				<v-divider class="mb-4"></v-divider>
+				<p class="text-body">Martin Luther posts his Ninety-five Theses on the door of the Castle Church in Wittenberg. This begins the protestant reformation, which includes theologians: Luther, Huldrych Zwingli, and Calvin.</p>
+			</div>
+		</v-timeline-item>
+
+		<v-timeline-item dot-color="red">
+			<div>
+				<div class="text-h6 mt-n1">c. 1685-1808 CE</div>
+				<v-divider class="mb-4"></v-divider>
+				<p class="text-body">Protestant Scholastics: Immanuel Kant, Johann Gottlieb Fichte, Friedrich Wilhelm Joseph Schelling, Friedrich Schleiermacher, David Strauss</p>
+			</div>
+		</v-timeline-item>
+		<v-timeline-item dot-color="blue-darken-1">
+			<div>
+				<div class="text-h6 mt-n1">c. 1729-1810 CE</div>
+				<v-divider class="mb-4"></v-divider>
+				<p class="text-body">Maskilim (Jewish Enlightenment): Moses Mendelssohn, Nahman Krochmal, Samson Raphael Hirsch, Abraham Geiger (founder of Reform Judaism)</p>
+			</div>
+		</v-timeline-item>
+		<v-timeline-item dot-color="blue-darken-1">
+			<div>
+				<div class="text-h6 mt-n1">c. 1842 CE</div>
+				<v-divider class="mb-4"></v-divider>
+				<p class="text-body">Hermann Cohen, founder of neo-Kantianism. Teacher of Franz Rosenzweig, and influences Martin Buber.</p>
+			</div>
+		</v-timeline-item>
+		<v-timeline-item dot-color="red">
+			<div>
+				<div class="text-h6 mt-n1">c. 1813-1889 CE</div>
+				<v-divider class="mb-4"></v-divider>
+				<p class="text-body">Existentialism: Søren Kierkegaard, Martin Heidegger</p>
+			</div>
+		</v-timeline-item>
+		<v-timeline-item dot-color="blue-darken-1">
+			<div>
+				<div class="text-h6 mt-n1">1878 CE</div>
+				<v-divider class="mb-4"></v-divider>
+				<p class="text-body">Martin Buber</p>
+			</div>
+		</v-timeline-item>
+		<v-timeline-item dot-color="red">
+			<div>
+				<div class="text-h6 mt-n1">c. 1886-1892 CE</div>
+				<v-divider class="mb-4"></v-divider>
+				<p class="text-body">Neo-Orthodoxy / Dialectical Theology: Karl Barth, Rudolf Bultmann, Dietrich Bonhoeffer, Reinhold Niebuhr</p>
+			</div>
+		</v-timeline-item>
+		<v-timeline-item dot-color="blue-darken-1">
+			<div>
+				<div class="text-h6 mt-n1">1907 CE</div>
+				<v-divider class="mb-4"></v-divider>
+				<p class="text-body">Abraham Joshua Heschel</p>
+			</div>
+		</v-timeline-item>
+		<v-timeline-item dot-color="red">
+			<div>
+				<div class="text-h6 mt-n1">1929 CE</div>
+				<v-divider class="mb-4"></v-divider>
+				<p class="text-body">Martin Luther King, Jr.</p>
+			</div>
+		</v-timeline-item>
+
 	</v-timeline>
-  </template>
+</template>
   
-  <script lang="ts" setup>
-  </script>
+<script lang="ts" setup>
+</script>
   
 <style lang="scss">
 @use 'vuetify/_settings';
