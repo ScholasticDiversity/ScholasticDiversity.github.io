a8cab8fbd6a6f89e0ca53ef02114d95f33ab18ef
Christian Seibold <krixano@outlook.com>
2023-06-27 06:32:29 -0500

    Add "Written Not By AI" badge to footer.

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,12 +1,16 @@
 <template>
     <v-main>
-        <div class="pa-6 pb-12" style="max-width: 900px; margin: auto;">
+        <div class="pa-6" style="max-width: 900px; margin: auto;">
             <router-view v-slot="{ Component }">
                 <keep-alive>
                     <component :is="Component" />
                 </keep-alive>
             </router-view>
         </div>
+        <v-footer class="pb-10" style="max-width: 900px; margin: auto;">
+            <a href="https://notbyai.fyi/" class="pr-4"><v-img inline src="@/assets/Not-By-AI/English/Written By Human/SVG/Written-By-Human-Not-By-AI-Badge-black.svg" height="40" alt="Written by Human, not by AI"></v-img></a>
+            <span>2023 — <strong>Scholastic Diversity</strong></span>
+        </v-footer>
     </v-main>
 </template>
 
