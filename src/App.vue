341fa86db1317dfcb28f6e220d565e45c419d753
Christian Seibold <christian.seibold@outlook.com>
2023-06-30 11:36:21 -0500

    Dynamically update theme when browser's expected theme changes (or when OS theme changes)

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,11 @@
 </template>
 
 <script lang="ts" setup>
-  //
+  import { useTheme } from 'vuetify/lib/framework.mjs';
+
+  const theme = useTheme();
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+      const newColorScheme = event.matches ? "dark" : "light";
+      theme.global.name.value = newColorScheme;
+  });
 </script>
